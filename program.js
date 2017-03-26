66a6b9603bd8c5e30eebab1335e618613cdeebaf
rounakbanik <rounakbanik@gmail.com>
2017-03-27 00:24:29 +0530

    Add solution to Server.

--- program.js
+++ program.js
@@ -1,15 +1,29 @@
 var express = require('express');
-var app = express();
+ var app = express();
 
-app.set('port', (process.argv[2] || 3000));
-app.set('view engine', 'jsx');
-app.set('views', __dirname + '/views');
-app.engine('jsx', require('express-react-views').createEngine({transformViews: false}));
+ app.set('port', (process.argv[2] || 3000));
+ app.set('view engine', 'jsx');
+ app.set('views', __dirname + '/views');
+ app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
 
-require('babel/register')({ignore: false});
+ require('babel/register')({
+     ignore: false
+ });
 
-app.use('/', function(req, res) {
-    res.render('index', '');
-});
+ // write below
+ var data = [
+   {
+      title: "Shopping",
+      detail: process.argv[3]
+   },
+   {
+      title: "Hair cut",
+      detail: process.argv[4]
+   }
+];
 
-app.listen(app.get('port'), function() {});
+ app.use('/', function(req, res) {
+   res.render('index', {data: data});
+ });
+
+ app.listen(app.get('port'), function() {});
--- views/index.jsx
+++ views/index.jsx
@@ -5,7 +5,7 @@ export default class TodoBox extends React.Component {
         return (
             <div className="todoBox">
                 <h1>Todos</h1>
-                <TodoList/>
+                <TodoList data={this.props.data}/>
                 <TodoForm/>
             </div>
         );
@@ -14,15 +14,16 @@ export default class TodoBox extends React.Component {
 
 class TodoList extends React.Component {
     render() {
+        var todo = this.props.data.map(function(obj) {
+            return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
+        });
         return (
             <div className="todoList">
                 <table style={{
                     border: "2px solid black"
                 }}>
                     <tbody>
-                        <Todo title="Shopping">Milk</Todo>
-                        <Todo title="Hair cut">13:00</Todo>
-                        <Todo title="Learn React">15:00</Todo>
+                        {todo}
                     </tbody>
                 </table>
             </div>
@@ -51,7 +52,7 @@ class Todo extends React.Component {
                     <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
                 </td>
                 <td style={style.tableContent}>{this.props.title}</td>
-            <td style={style.tableContent}>{this.props.children}</td>
+                <td style={style.tableContent}>{this.props.children}</td>
             </tr>
         );
     }
